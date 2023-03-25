521027a687d5af17be3cac6e630b36fd4652d62a
Daniel <denamudu@bariansystems.com>
2023-03-25 21:31:41 +0100

    I have added all routes guards in the route/index.js file.

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,50 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../pages/Home.vue';
 import LoginPage from '../pages/Login.vue';
 import RegisterPage from '../pages/Register.vue';
+import useUserStore from '../store/user';
 
 const routes = [
     {
         path: '/', 
-        component: HomePage
+        component: HomePage,
+        beforeEnter: (to, from, next) => {
+            // Install the user store
+            const userStore = useUserStore();
+            // Redirect if user is not authenticated
+            if (userStore.userIsAuth === false) {
+                return next('/login');
+            }
+            // Allow route entry if user is authenticated
+            return next();
+        }
     },
     {
         path: '/login', 
-        component: LoginPage
+        component: LoginPage,
+        beforeEnter: (to, from, next) => {
+            // Install the user store
+            const userStore = useUserStore();
+            // Redirect if user is authenticated
+            if (userStore.userIsAuth === true) {
+                return next('/');
+            }
+            // Allow route entry if user is not authenticated
+            return next();
+        }
     },
     {
         path: '/register', 
-        component: RegisterPage
+        component: RegisterPage,
+        beforeEnter: (to, from, next) => {
+            // Install the user store
+            const userStore = useUserStore();
+            // Redirect if user is authenticated
+            if (userStore.userIsAuth === true) {
+                return next('/');
+            }
+            // Allow route entry if user is not authenticated
+            return next();
+        }
     }
 ];
 
