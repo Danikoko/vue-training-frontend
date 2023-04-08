8041d507eecec279b319e050052cc55abef86fb8
Daniel <denamudu@bariansystems.com>
2023-04-08 23:06:26 +0100

    I updated the API URL.

--- src/store/general.js
+++ src/store/general.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 const useGeneralStore = defineStore('general', {
     state: () => ({ 
-        API_URL: 'http://localhost:8000/api/'
+        API_URL: 'http://vue-training-backend.danikoko.com/api/'//'http://localhost:8000/api/'
     })
 });
 
